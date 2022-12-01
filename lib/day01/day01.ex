dcf6efed2cc01055927b32d6cef81c3508ab1fb7
Erik Karlstrand <erik.jonsson17@gmail.com>
2022-12-01 20:55:37 +0100

    Solutions for day 1

--- lib/day01/day01.ex
+++ lib/day01/day01.ex
@@ -1,7 +1,19 @@
 defmodule Day01 do
-  import Util
+  import Util, only: [file_to_jagged_arr: 4]
 
   def part_one(input) do
+    elves = file_to_jagged_arr(input, "", false, fn x -> String.to_integer(x) end)
+    Enum.map(elves, fn x -> Enum.sum(x) end) |> Enum.sort(:desc) |> hd
+  end
 
+  def part_two(input) do
+    elves = file_to_jagged_arr(input, "", false, fn x -> String.to_integer(x) end)
+    Enum.map(elves, fn x -> Enum.sum(x) end) |> Enum.sort(:desc) |> Enum.take(3) |> Enum.sum
+  end
+
+  def run do
+    {:ok, input} = File.read("lib/day01/input")
+    IO.puts(part_one(input))
+    IO.puts(part_two(input))
   end
 end
--- test/day01/day01_test.exs
+++ test/day01/day01_test.exs
@@ -3,6 +3,16 @@ defmodule Day01Test do
   doctest Day01
 
   test "gets correct result for part 1" do
+    input = "1000\n2000\n3000\n\n4000\n\n5000\n6000\n\n7000\n8000\n9000\n\n10000"
+    expected = 24000
 
+    assert Day01.part_one(input) === expected
+  end
+
+  test "gets correct result for part 2" do
+    input = "1000\n2000\n3000\n\n4000\n\n5000\n6000\n\n7000\n8000\n9000\n\n10000"
+    expected = 45000
+
+    assert Day01.part_two(input) === expected
   end
 end
